27bd1672dc2a6f16ab6f9f3b3a24f0e01ed4efc9
Tin Chu <mchu.tinho@gmail.com>
2017-11-29 09:21:11 -0600

    increment postCount by 1

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating Records', () => {
   let joe;
 
   beforeEach( done => {
-    joe = new User({ name: 'Joe'});
+    joe = new User({ name: 'Joe', postCount: 0 });
     joe.save()
       .then(() => done());
   });
@@ -40,5 +40,14 @@ describe('Updating Records', () => {
 
   it('A model class can find a record with an Id and update', done => {
     assertName(User.findByIdAndUpdate(joe._id, { name: 'Alex' }), done);
+  });
+
+  it('A user can have their postcount incremented by 1', done => {
+    User.update({name: 'Joe'}, { $inc: { postCount: 1 } })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then(user => {
+        assert(user.postCount === 1);
+        done();
+      });
   });
 });

09a2088249418b3e45e1c9d6770a2593eae7fadc
Tin Chu <mchu.tinho@gmail.com>
2017-11-29 10:13:36 -0600

    validation errors

---
+++ test/validation_test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Validating records', () => {
+  it('requires a username', done => {
+    const user = new User({ name: undefined });
+
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
+
+    assert(message === 'Name is required.');
+    done();
+  });
+});

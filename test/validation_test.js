d2e9fb592312ec4bc89eb7a9800c56bc5c7e080c
Tin Chu <mchu.tinho@gmail.com>
2017-12-18 10:39:57 -0600

    subdocuments .save()

--- test/validation_test.js
+++ test/validation_test.js
@@ -2,13 +2,38 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Validating records', () => {
+
+  let user;
+
+  function validationResults(user) {
+    let validationResult = user.validateSync();
+    let { message } = validationResult.errors.name;
+    return message;
+  };
+
   it('requires a username', done => {
-    const user = new User({ name: undefined });
+    user = new User({ name: undefined });
 
-    const validationResult = user.validateSync();
-    const { message } = validationResult.errors.name;
-
-    assert(message === 'Name is required.');
+    assert(validationResults(user) === 'Name is required.');
     done();
+  });
+
+  it('requires a user name longer than 2 characters', done => {
+    user = new User({ name: 'Al'});
+
+    assert(validationResults(user) === 'Name must be longer than 2 characters.');
+    done();
+  });
+
+  it('disallows invalid records from being saved', done => {
+    user = new User({ name: 'Al' });
+
+    user.save()
+      .catch( (validationResult) => {
+        const { message } = validationResult.errors.name;
+
+        assert( message === 'Name must be longer than 2 characters.');
+        done();
+      });
   });
 });
